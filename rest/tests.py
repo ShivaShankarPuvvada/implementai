80de0ac76891635f568ce8e85d52607de78ba6c3
ShivaShankarPuvvada <om.ss.sankar@gmail.com>
2020-02-14 22:19:38 +0530

    added testing

--- rest/tests.py
+++ rest/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from rest.models import Rack, Student
 
-# Create your tests here.
+from rest_framework.test import APIRequestFactory
+
+factory = APIRequestFactory()
+
+
+request = factory.post('/racks/', {'title': 'R33'})
+request = factory.put('/racks/12/', {'title': 'R11'})
+
+
+request = factory.post('/students/', {'name': 'S6'})
+
+# testing manytomanyfield
+racks = ['R1', 'R2', 'R3']
+request = factory.put('/students/4/', {'name': 'S10', 'rack': racks})
+
